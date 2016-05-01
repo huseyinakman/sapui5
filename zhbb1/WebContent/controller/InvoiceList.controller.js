c4108b366a9af89f84a59798c9fbe917469258f2
huseyin.akman <huseyin.akman@gmail.com>
2016-05-01 15:15:11 +0300

    Step 24

--- zhbb1/WebContent/controller/InvoiceList.controller.js
+++ zhbb1/WebContent/controller/InvoiceList.controller.js
@@ -1,17 +1,30 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/model/formatter"
-], function (Controller, JSONModel, formatter) {
+sap.ui.define([ "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel",
+		"sap/ui/demo/wt/model/formatter", "sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator" ], function(Controller, JSONModel,
+		formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.wt.controller.InvoiceList", {
-		formatter: formatter,	
-		onInit : function () {
+		formatter : formatter,
+		onInit : function() {
 			var oViewModel = new JSONModel({
-				currency: "EUR"
+				currency : "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+		onFilterInvoices : function(oEvent) {
+			// build filter array
+			var aFilter = [];
+			var sfilter = this.getView().byId("filterid").getValue();
+//			var sQuery = oEvent.getParameter("query");
+			if (sfilter) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sfilter));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		}
 
 	});
